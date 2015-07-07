5c2bd78a83095bde8e3b3a17a07bcdb96d2d13e9
changjoo jeon <changjoojeon@gmail.com>
2015-07-07 16:44:55 -0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 		port:process.env.PORT || 3030
 	},
 	production:{
-		db:'mongodb://boyntonchang:julielove@ds053168.mongolab.com:53168/pilgrim',
+		db:'mongodb://boyntonchang:julielove423@ds061721.mongolab.com:61721/heroku_app35593598',
 		rootPath:rootPath,
 		port:process.env.PORT || 80 
 	}
